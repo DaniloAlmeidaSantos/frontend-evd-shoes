2b375476335a4e02786044ceb2e3f80a74b0c904
DANILO ALMEIDA DOS SANTOS <danilo.asantos27@senacsp.edu.br>
2023-03-07 20:46:15 -0300

    Initials commit to new feature

---
+++ src/screens/backofficeHomeScreen/BackofficeHomeScreen.js
@@ -0,0 +1,16 @@
+import './BackofficeHomeScreen.css';
+
+function BackofficeHomeScreen() {
+  return (
+    <body>
+      <div class="container">
+
+        <div class="sizeBox"></div>
+        <div class="sizeBox"></div>
+
+      </div>
+    </body>
+  );
+}
+
+export default BackofficeHomeScreen;
