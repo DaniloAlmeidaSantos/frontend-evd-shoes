02cc1f24ff6c7bbe67b6a8f303c2319311582f7b
DaniloAlmeidaSantos <danilo8almeida@hotmail.com>
2023-03-08 21:41:10 -0300

    Adding BackofficeHomeScreen finished

--- src/screens/backofficeHomeScreen/BackofficeHomeScreen.js
+++ src/screens/backofficeHomeScreen/BackofficeHomeScreen.js
@@ -1,14 +1,30 @@
 import './BackofficeHomeScreen.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUsers, faBoxesStacked } from '@fortawesome/free-solid-svg-icons'
 
 function BackofficeHomeScreen() {
   return (
     <body>
-      <div class="container">
-
-        <div class="sizeBox"></div>
-        <div class="sizeBox"></div>
-
-      </div>
+      <main class="container">
+        <header class="header">
+          <h1>BACKOFFICE</h1>
+          <p>Escolha qual rotina deseja selecionar</p>
+        </header>
+        <section class="boxes">
+          <section class="options">
+            <div class="logos">
+              <FontAwesomeIcon size="9x" icon={faUsers} inverse />
+            </div>
+            <h3>Gerenciar usuários</h3>
+          </section>
+          <section class="options">
+            <div class="logos">
+              <FontAwesomeIcon size="9x" icon={faBoxesStacked} inverse />
+              <h3>Gerenciar produtos</h3>
+            </div>
+          </section>
+        </section>
+      </main>
     </body>
   );
 }
