7abb21bed4de3fe3c08d2588bb38af621aa15eda
elviswlima <elvis.wlima@hotmail.com>
2023-03-09 01:24:51 -0300

    criado screen userlist, favor verificar erro nas tags, não consegui identificar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+//import UserListScreen from './screens/UserListScreen';
 import Login from './screens/LoginScreen/Login';
 import reportWebVitals from './reportWebVitals';
 
---
+++ src/screens/UserListScreen/UserListScreen.js
@@ -0,0 +1,65 @@
+import './UserListScreen.css';
+
+function UserListScreen() {
+    return(
+        <body>
+        <main class="bloco">
+            <div>
+                <h1>Lista de usuários</h1>
+            </div>
+            <div class="bloco-botao">
+                <button class="botao">Cadastrar usuário</button>
+            </div>
+        </main>
+        <section>
+            <table>
+                <tr class="titulo-tabela">
+                    <th>Nome do usuário</th>
+                    <th>E-mail</th>
+                    <th>Grupo</th>
+                    <th>Status</th>
+                    <th>Ações</th>
+                </tr>
+                <tr>
+                    <td>Nome do usuário</td>
+                    <td>E-mail</td>
+                    <td>Grupo</td>
+                    <td>
+                        <input type="radio" id="on" value="1">on
+                        <input type="radio" id="off" value="0">off
+                    </td>
+                    <td>
+                        <button class="botao-editar"><i class="fa fa-solid fa-pen"></i></button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Nome do usuário</td>
+                    <td>E-mail</td>
+                    <td>Grupo</td>
+                    <td>
+                        <input type="radio" id="on" value="1">on
+                        <input type="radio" id="off" value="0">off
+                    </td>
+                    <td>
+                        <button class="botao-editar"><i class="fa fa-solid fa-pen"></i></button>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Nome do usuário</td>
+                    <td>E-mail</td>
+                    <td>Grupo</td>
+                    <td>
+                        <input type="radio" id="on" value="1">on
+                        <input type="radio" id="off" value="0">off
+                    </td>
+                    <td>
+                        <button class="botao-editar"><i class="fa fa-solid fa-pen"></i></button>
+                    </td>
+                </tr>
+            </table>
+        </section>
+    </body>
+    );
+}
+
+export default UserListScreen;
