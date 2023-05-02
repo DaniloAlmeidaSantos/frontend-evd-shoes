5fc61485a82f303a7196db4a0b2d646a60b7ca60
DaniloAlmeidaSantos <danilo8almeida@hotmail.com>
2023-05-01 21:40:42 -0300

    Adjust greeting message

--- src/utils/HoursUtils.js
+++ src/utils/HoursUtils.js
@@ -2,15 +2,20 @@ const userInfo = JSON.parse(localStorage.getItem('userInfo'));
 
 const greetingMessage = () => {
     let h = new Date().toLocaleTimeString('pt-BR', { hour: 'numeric', hour12: false }); // formato 24 horas (0-23)
+    let message;
     if (h >= 0 && h <= 5) { // entre meia noite (0h) e 5 da madrugada
-        return 'Boa madrugada, ' + userInfo.username;
+        message = 'Boa madrugada';
     } else if (h >= 6 && h < 12) { // entre 6 e 11 da manhã
-        return 'Bom dia, ' + userInfo.username; ;
+        message = 'Bom dia';
     } else if (h >= 12 && h < 18) { // entre meio dia (12h) e 17 (5h) da tarde
-        return 'Boa tarde, ' + userInfo.username;;
+        message = 'Boa tarde';
     } else if (h >= 18 && h <= 23) { // entre 18 (6h) e 23 (11h) da noite
-        return 'Boa noite, ' + userInfo.username;;
+        message = 'Boa noite';
     }
+
+    if (userInfo != null) message = message + `, ${userInfo.username}`;
+
+    return message;
 }
 
 export default greetingMessage;
